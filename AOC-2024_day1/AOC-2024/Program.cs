62a3c6cc7154701ba33055d0f3adbd0ab6cdc601
dunderhugo <forssellhugo@gmail.com>
2024-12-02 02:16:54 +0100

    Day 1 part one

---
+++ AOC-2024_day1/AOC-2024/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Runtime.ExceptionServices;
+
+namespace AOC_2024
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = "..\\..\\..\\input.txt";
+			List<int> leftValue = new List<int>();
+			List<int> rightValue = new List<int>();
+			List<int> sumValue = new List<int>();
+			string[] allLines = File.ReadAllLines(filePath);
+            Console.WriteLine("Alllines count: " + allLines.Length);
+            foreach (string line in allLines)
+			{
+                string [] splitLine = line.Split("   ");
+				leftValue.Add(int.Parse(splitLine[0]));
+				rightValue.Add(int.Parse(splitLine[1]));
+			}
+            Console.WriteLine("right count: " + rightValue.Count());
+            leftValue.Sort();
+			rightValue.Sort();
+			int sum = 0;
+			for(int i = 0; i < leftValue.Count(); i++)
+			{
+				sum = leftValue[i] - rightValue[i];
+				if(sum < 0) sum = Math.Abs(sum);
+				sumValue.Add(sum);
+			}
+			int totalSum = 0;
+			foreach(int i in sumValue)
+			{
+				totalSum += i;
+			}
+			Console.WriteLine(totalSum);
+
+        }
+
+
+	}
+}
